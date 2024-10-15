75b28d11f4a8a5668bfff7abb13c2bd0e5af42e9
Jesus-Lapto <jrjimenez296@hotmail.com>
2024-10-15 19:59:05 -0300

    .

--- Ejercicos/Ejercicio 5.py
+++ Ejercicos/Ejercicio 5.py
@@ -4,24 +4,25 @@ class Universidad:
         self.nameuniversidad = nameuniversidad
 
 
-class UniversidadCarrera:
+class Carrera:
     def __init__(self, especialidad):
         self.especialidad = especialidad
 
 
-class UniversidadEstudiante(Universidad, UniversidadCarrera):
+class Estudiante(Universidad, Carrera):
     def __init__(self, nombre, apellido, edad, nameuniversidad, especialidad):
-        Universidad.__init__(nameuniversidad)
-        UniversidadCarrera.__init__(especialidad)
+        super().__init__(nameuniversidad)
+        super().__init__(especialidad)
         self.nameuniversidad = nameuniversidad
         self.especialidad = especialidad
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
+        
     def __str__(self):
         return f"El estudiante de nombre y apellido {self.nombre} {self.apellido} de edad {self.edad} años, se encuentra en la universidad {self.nameuniversidad} cursando {self.especialidad}"
     
 
-persona = UniversidadEstudiante("Jesus", "Jimenez", 23, "UNICEN", "Ingeneria de Sistema")
+persona =Estudiante("Jesus", "Jimenez", 23, "UNICEN", "Ingeneria de Sistema")
 
 print(persona)
